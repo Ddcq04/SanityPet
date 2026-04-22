6dc0d4c4a05e15dae66f1dfb2dc79cc45e797e2b
Diego <diegochauca15@gmail.com>
2026-04-22 18:26:35 +0200

    Cambios en security config

--- src/main/java/com/veterinaria/gestion/config/SecurityConfig.java
+++ src/main/java/com/veterinaria/gestion/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
 	    }
 	    @Bean
 	    public PasswordEncoder passwordEncoder() {
-	       return new BCryptPasswordEncoder(); // Para manejar contraseñas seguras
+	    	return org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance();
+	    	//return new BCryptPasswordEncoder(); // Para manejar contraseñas seguras
 	    }
 	}
